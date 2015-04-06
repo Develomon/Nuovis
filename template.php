436d90e86065f7d87827d6a69f040b93b127fb44
Dragan Eror <draganeror@gmail.com>
2015-04-06 13:28:58 +0200

    Created theme structure.

---
+++ template.php
@@ -0,0 +1,52 @@
+<?php
+
+
+/*** PREPROCESS FUNCTIONS ***/
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess(&$variables, $hook) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_html(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_page(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_entity(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_node(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_region(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_block(&$variables) {
+}
+
+/**
+ * Implementation of template_preprocess().
+ */
+function nuovis_preprocess_field(&$variables) {
+}
