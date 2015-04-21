c3b219989d56241a7ae87beea1478697d231d520
Dragan Eror <draganeror@gmail.com>
2015-04-21 19:52:45 +0200

    Added sass files. Changed tabs classes to support BEM style. #1

--- template.php
+++ template.php
@@ -50,3 +50,33 @@ function nuovis_preprocess_block(&$variables) {
  */
 function nuovis_preprocess_field(&$variables) {
 }
+
+/*** PROCESS FUNCTIONS ***/
+
+/*** ALTER FUNCTIONS ***/
+
+/*** TEHEME FUNCTIONS ***/
+
+/**
+ * Override theme_menu_local_tasks().
+ */
+function nuovis_menu_local_tasks(&$variables) {
+  $output = '';
+
+  if (!empty($variables ['primary'])) {
+    $variables ['primary']['#prefix'] = '<h2 class="tabs__title element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables ['primary']['#prefix'] .= '<ul class="tabs tabs--primary">';
+    $variables ['primary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables ['primary']);
+  }
+  if (!empty($variables ['secondary'])) {
+    $variables ['secondary']['#prefix'] = '<h2 class="tabs__title element-invisible">' . t('Secondary tabs') . '</h2>';
+    $variables ['secondary']['#prefix'] .= '<ul class="tabs tabs--secondary">';
+    $variables ['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables ['secondary']);
+  }
+
+  return $output;
+}
+
+/*** HELPER FUNCTIONS ***/
